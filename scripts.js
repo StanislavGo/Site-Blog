d4b05bdc747044ddc888c5322d221eafff70bf51
StanislavGo <gorokht6mail.ru@gmail.com>
2023-10-02 15:27:49 +0300

    feat: add functionality of burger menu

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+// FUNCTION FOR OPEN BURGER MODAL WINDOW
+function openBurgerMenu() {
+    $(".burger-menu").toggleClass('active');
+
+    if ($(".burger-menu").hasClass('active')) {
+        $(".burger-modal-window").css({ "right": "0px" });
+        $("body").css({ "overflow": "hidden" });
+        $(".overlay-when-burger-is-open").css({ "display": "block" });
+    } else {
+        $(".burger-modal-window").css({ "right": "-100%" });
+        $("body").css({ "overflow": "auto" });
+        $(".overlay-when-burger-is-open").css({ "display": "none" });
+    }
+}
+
+function closeOverlayForBurger() {
+    $(".burger-modal-window").css({ "right": "-100%" });
+    $("body").css({ "overflow": "auto" });
+    $(".overlay-when-burger-is-open").css({ "display": "none" });
+
+    $(".burger-menu").removeClass("active");
+}
