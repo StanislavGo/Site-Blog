9d304e0d9b9f75619155485576d5a1d70e9ec38b
Sergey <rogovoyserg2006@gmail.com>
2023-10-04 10:42:14 +0300

    Added Section Client

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,6 @@
 const swiperOne = new Swiper('.project_swiper', {
     loop: true,
-    // slidesPerView: 2,
+
     breakpoints: {
         // 640: {
         //   slidesPerView: 2,
@@ -13,4 +13,21 @@ const swiperOne = new Swiper('.project_swiper', {
           slidesPerView: 2,
         },
       },
+});
+
+const swiper = new Swiper(".slide-conteiner", {
+  loop: true,
+  breakpoints:{
+    640:{
+      slidesPerView: 1,
+      centerSlide: true,
+    },
+    1300:{
+      slidesPerView: 2,
+      centerSlide: true,
+    },
+    1500:{
+      slidesPerView: 3,
+    },
+  },
 });
