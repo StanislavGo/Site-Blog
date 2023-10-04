cddcee8b60d2fbd4c5e448105e06b5bd092107f6
StanislavGo <gorokht6mail.ru@gmail.com>
2023-10-04 23:47:55 +0300

    add animation while scrolling

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,8 +1,3 @@
-$(document).ready(function() {
-    showNameAndProfessionsFirstSection();
-    showPhotoFromFirstSection();
-});
-
 function showNameAndProfessionsFirstSection() {
     $(".name-and-surname").addClass("active");
     $(".who-I-am").addClass("active");
@@ -13,3 +8,35 @@ function showPhotoFromFirstSection() {
         "transform": "translate(0px, 0px)", 
         "opacity": "1"});
 }
+
+function showHeader() {
+    $(".header").css({ 
+        "transform": "translate(0px, 0px)",
+        "opacity": "1"
+     });
+}
+
+function showElements() {
+    $(".element").addClass("active");
+}
+
+$(document).ready(function() {
+    showNameAndProfessionsFirstSection();
+    showPhotoFromFirstSection();
+    showHeader();
+    showElements();
+});
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
