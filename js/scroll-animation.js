2c2d7a71954de81a18fcdee6d395d7cd4e50fbd2
StanislavGo <gorokht6mail.ru@gmail.com>
2023-10-07 22:29:46 +0300

    feat: add function that shows which section is choosen

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -36,10 +36,46 @@ const observer = new IntersectionObserver((entries) => {
             entry.target.classList.remove("show");
         }
     });
+    checkIfElementHasClassShow();
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+function rejectAllChoseClasses() {
+    let elements = document.querySelectorAll(".choose");
+    elements.forEach(e => {
+        e.classList.remove("choose");
+    })
+}
+
+// Function to show which section is choosen
+function checkIfElementHasClassShow() {
+    rejectAllChoseClasses();
+    if (document.querySelector(".homeForCheck").classList.contains("show")) {
+        document.querySelector('#home1').className = "choose";
+    }
+    if (document.querySelector(".experienceCheck").classList.contains("show")) {
+        document.querySelector('#skills1').className = "choose";
+    }
+    if (document.querySelector(".projectsForCheck").classList.contains("show")) {
+        document.querySelector("#projectsForCheck").className = "choose";
+    }
+    if (document.querySelector(".clientsForCheck").classList.contains("show")) {
+        document.querySelector("#clientsForCheck").className = "choose";
+    }
+    if (document.querySelector(".FAQForCheck").classList.contains("show")) {
+        document.querySelector("#FAQForCheck").className = "choose";
+    }
+    if (document.querySelector(".contactForCheck").classList.contains("show")) {
+        document.querySelector("#contactForCheck").className = "choose";
+    }
+    if (document.querySelector(".blogForCheck").classList.contains("show")) {
+        document.querySelector("#blogForCheck").className = "choose";
+    }
+}
+
+console.log(document.querySelector(".experience-block.hidden"));
 
 // Scroll animation
 $("a[href*='#']").click(function() {
